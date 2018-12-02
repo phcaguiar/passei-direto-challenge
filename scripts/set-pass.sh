ff27e48fd2f6c28baf9ae0c44891346adba90926
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2018-12-02 06:03:13 +0000

    Removing password script

--- scripts/set-pass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-notesapi=$(cat variables | awk '{print$3}' | awk 'NR == 5')
-mysqlroot=$(cat variables | awk '{print$3}' | awk 'NR == 9')
-
-
-sed -i "s|{note-api-pass}|$notesapi|g" docker-compose/node/server.js
-sed -i "s|{note-api-pass}|$notesapi|g" docker-compose/mysql/Dockerfile
-sed -i "s|{mysql-root-pass}|$mysqlroot|g" docker-compose/mysql/Dockerfile
